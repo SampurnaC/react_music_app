f91b8156752190108ce8da4f02e28acfa16c1fff
sam <sampurna.chapagainnn@gmail.com>
2022-08-02 10:33:05 +0100

    add prop from app component to artist component

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Artist = () => {
+const Artist = ({ genres }) => {
+  const [name, setName] = useState('');
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`${name}`);
+  };
   return (
     <div className="artist_container">
       <h1>Linkin Park</h1>
-      <h2>Rock, Rap, Electronic, Alternative</h2>
+      {genres.map((genre) => genre)}
       <p>
         Linkin Park is an American rock band from California. The band's current
-        lineup cromprises of vocalist/rhythm guitarist/keyboardist Mike SHinoda,
+        lineup cromprises of vocalist/rhythm guitarist/keyboardist Mike Shinoda,
         lead guitarist Brad Delson, bassist Dave Farrell, DJ/turntablist Joe
         Hahn and drummer Rob Bourdon, all of whom are founding members.
       </p>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your name:</label>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
