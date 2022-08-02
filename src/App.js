eceae42597761f0e5bdc241a891d11b74f44731a
sam <sampurna.chapagainnn@gmail.com>
2022-08-02 11:08:00 +0100

    add counter component and add events to it

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Artist from './components/Artist';
 import Song from './components/Song';
+import { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
   const genres = ['Rock, ', 'Rap, ', 'Electronic, ', 'Alternative '];
-
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <Artist genres={genres}></Artist>
       <Song></Song>
+      <Counter></Counter>
     </div>
   );
 }
