a2fcf5b652648f8a5e82757a4035e4d165111027
sam <sampurna.chapagainnn@gmail.com>
2022-08-02 12:50:55 +0100

    add like dislike feature

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,27 +1,45 @@
 import React, { useState } from 'react';
 
-const Song = () => {
-  const [like, setLike] = useState(true);
+const Song = ({ likeMe, disLikeMe, like }) => {
+  //   const [like, setLike] = useState(true);
   const songs = [
-    { title: 'Faint', imageUrl: require('../images/faint.jpg') },
-    { title: 'New Divide', imageUrl: require('../images/newDivide.jpg') },
-    { title: 'Numb', imageUrl: require('../images/numb.jpg') },
-    { title: "What I've done", imageUrl: require('../images/WhatIveDone.jpg') },
+    {
+      id: 1,
+      title: 'Faint',
+      imageUrl: require('../images/faint.jpg'),
+      like: false,
+    },
+    {
+      id: 2,
+      title: 'New Divide',
+      imageUrl: require('../images/newDivide.jpg'),
+      like: false,
+    },
+    {
+      id: 3,
+      title: 'Numb',
+      imageUrl: require('../images/numb.jpg'),
+      like: false,
+    },
+    {
+      id: 4,
+      title: "What I've done",
+      imageUrl: require('../images/WhatIveDone.jpg'),
+      like: false,
+    },
   ];
   return (
     <div className="song_container">
       <div className="song">
         {songs.map((song) => (
-          <div key={song.title}>
+          <div key={song.id}>
             <h1>{song.title}</h1>
             <img src={song.imageUrl} alt="" />
+            {like}
+            {like === false && <button onClick={likeMe}>Like</button>}
+            {like === true && <button onClick={disLikeMe}>Dislike</button>}
           </div>
         ))}
-
-        {/* {like === true && <button onClick={() => setLike(false)}>Like</button>}
-        {like === false && (
-          <button onClick={() => setLike(true)}>Dislike</button>
-        )} */}
       </div>
     </div>
   );
